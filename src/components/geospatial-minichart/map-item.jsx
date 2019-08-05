95ae96b88c46664e22612f22970d1a0ac8671d7f
Lucas Hrabovsky <hrabovsky.lucas@gmail.com>
2019-08-05 01:13:50 -0400

    feat: rewrite map item

--- src/components/geospatial-minichart/map-item.jsx
+++ src/components/geospatial-minichart/map-item.jsx
@@ -1,82 +1,235 @@
+/* eslint-disable valid-jsdoc */
+/* eslint-disable react/sort-comp */
+/* eslint-disable no-console */
+/* eslint-disable quotes */
+/* eslint-disable no-unused-vars */
+
+import React, { PureComponent } from "react";
+import PropTypes from "prop-types";
+import has from "lodash.has";
+import get from "lodash.get";
+
+import { Map, TileLayer } from "react-leaflet";
+import "leaflet/dist/leaflet.css";
+import "leaflet-defaulticon-compatibility/dist/leaflet-defaulticon-compatibility.webpack.css"; // Re-uses images from ~leaflet package
+import "leaflet-defaulticon-compatibility";
+
+import GeoscatterMapItem from "./geoscatter-map-item";
+
+import { DEFAULT_TILE_URL } from "./constants";
+import { getHereAttributionMessage } from "./utils";
+
+const SELECTED_COLOR = '#F68A1E';
+const UNSELECTED_COLOR = '#43B1E5';
+const CONTROL_COLOR = '#ed271c';
+
 /**
- * NOTE: (lucas) for why `AppContext` related stubbing and
- * test cases are commented out, please see:
- * https://github.com/10gen/compass-schema/pull/2
+ * Fetches the tiles from the compass maps-proxy
+ * and attaches the attribution message to the
+ * map.
+ * @param {react-leaflet.Map} map The rendered component ref.
  */
-import React, { PureComponent } from 'react';
-import PropTypes from 'prop-types';
-import { find, isArray, isNumber } from 'lodash';
-import { Map, TileLayer } from 'react-leaflet';
+const attachAttribution = async function(map) {
+  let attributionMessage = "";
+  if (map) {
+    const bounds = map.leafletElement.getBounds();
+    const level = map.leafletElement.getZoom();
 
-import GeoscatterMapItem from './geoscatter-map-item';
+    attributionMessage = await getHereAttributionMessage(bounds, level);
+  }
+  return attributionMessage;
+};
 
-import { DEFAULT_TILE_URL } from './constants';
-import { MAP_ROLES, GeoSpatialScatterRole } from './map-types';
+/**
+ * Configuration options for leaflet.
+ */
+const LEAFLET_OPTIONS = {
+  // center: [-76.4689578, 39.2620277],
+  // zoom: 13
+};
 
-import { getHereAttributionMessage } from './utils';
+// /**
+//  * Conversion for display in minicharts for non-promoted BSON types.
+//  * TODO: lucas Make shared properly with
+//  */
+// const TO_JS_CONVERSIONS = {
+//   'Double': (values) => values.map((v) => v.value),
+//   'Int32': (values) => values.map((v) => v.value),
+//   'Long': (values) => values.map((v) => v.toNumber()),
+//   'Decimal128': (values) => values.map((v) => v.toString())
+// };
+
+/**
+ * Single doc of what Charts components accept.
+ */
+const DATA = {
+  documents: [
+    {
+      geopoint: {
+        type: "Point",
+        coordinates: [-76.4689578, 39.2620277],
+        center: [-76.4689578, 39.2620277],
+        color: "red",
+        fillOpacity: 0.5,
+        weight: 1,
+        fields: [
+          {
+            key: "Location",
+            value: "[-76.4689578, 39.2620277]"
+          }
+        ]
+      }
+    }
+  ],
+  metadata: {}
+};
+
+/**
+ * Conversion for display in minicharts for non-promoted BSON types.
+ * TODO: lucas - dedupe from d3Component.
+ */
+const TO_JS_CONVERSIONS = {
+  'Double': (v) => v.value,
+  'Int32': (v) => v.value,
+  'Long': (v) => v.toNumber(),
+  'Decimal128': (v) => v.toString()
+};
+
+/**
+ * TODO: lucas - 2 versions of bson causing values to have _bsontype or _bsonType.
+ */
+
+const valuesToLegacyLatLong = (values) => {
+  return values.map(function(v) {
+    if (!has(v, "_bsontype") && !has(v, "_bsonType")) {
+      return v;
+    }
+    const t = get(v, '_bsontype', get(v, '_bsonType'));
+    const primitive = TO_JS_CONVERSIONS[t](v);
+    return primitive;
+  });
+};
+/**
+ * Transforms an array `[lat,long]` coordinates into a GeoJSON Point.
+ */
+const valueToGeoPoint = values => {
+  const latLong = valuesToLegacyLatLong(values);
+  const point = {
+    type: "Point",
+    coordinates: latLong,
+    center: latLong,
+    color: UNSELECTED_COLOR,
+    /**
+     * Passed to `<CustomPopup />`
+     */
+    fields: [
+      {
+        key: "Location",
+        value: latLong.toString()
+      }
+    ]
+  };
+  return point;
+};
+
+/**
+ * Example `type` prop:
+ * 
+ * ```
+ * {
+ *   name: 'Boolean',
+ *   count: 1,
+ *   probability: 0.25,
+ *   unique: 1,
+ *   values: [true]
+ * }
+ * ```
+ */
+const TYPE_SHAPE = {
+  name: PropTypes.string.isRequired,
+  count: PropTypes.number.isRequired,
+  probability: PropTypes.number.isRequired,
+  unique: PropTypes.number,
+  values: PropTypes.array
+};
 
 class MapItem extends PureComponent {
+  static displayName = 'MapItemComponent';
   static propTypes = {
     _id: PropTypes.string,
     data: PropTypes.shape({
       documents: PropTypes.array,
-      metadata: PropTypes.object,
-    }).isRequired,
+      metadata: PropTypes.object
+    }),
     spec: PropTypes.object.isRequired,
-    chartType: PropTypes.string,
-    className: PropTypes.string,
-    onViewportChanged: PropTypes.func,
-    isResizing: PropTypes.bool,
+    type: PropTypes.shape(TYPE_SHAPE),
+    width: PropTypes.number.isRequired,
+    height: PropTypes.number.isRequired,
+    fieldName: PropTypes.string.isRequired
   };
 
   static defaultProps = {
-    isResizing: false,
+    spec: LEAFLET_OPTIONS,
+    data: DATA
   };
 
   state = {
     ready: false,
-    attributionMessage: '',
+    attributionMessage: ""
   };
 
   componentDidMount() {
     const { map } = this.refs;
-
-    if (map) {
-      // Called on zoom and pan
-      map.leafletElement.on('moveend', this.onViewportChanged);
-      const {
-        spec: { viewport },
-      } = this.props;
-
-      if (viewport && !this.props.spec.autoViewport) {
-        map.leafletElement.setView(viewport.center, viewport.zoom);
-      }
+    if (!map) {
+      return;
     }
+
+    /**
+     *  Called on zoom and pan
+     */
+    map.leafletElement.on("moveend", this.onViewportChanged.bind(this));
+    const {
+      type: { values }
+    } = this.props;
+
+
+    const latLongs = values.map((v) => valuesToLegacyLatLong(v));
+    map.leafletElement.fitBounds(latLongs);
   }
 
   componentDidUpdate() {
+    const { values } = this.props.type;
+    const latLongs = values.map((v) => valuesToLegacyLatLong(v));
+    const leaflet = this.refs.map.leafletElement;
+
+    // Sets a map view that contains the given geographical bounds with the maximum zoom level possible.
+    leaflet.fitBounds(latLongs);
+    // leaflet.flyToBounds(latLongs);
+    // leaflet.fitWorld();
+
     this.invalidateMapSize();
   }
 
-  onViewportChanged = () => {
-    if (!this.state.ready) {
-      return;
-    }
+  onViewportChanged() {
+    // if (!this.state.ready) {
+    //   return;
+    // }
 
-    const { map } = this.refs;
+    // const { map } = this.refs;
 
-    if (!this.props.onViewportChanged || !map) {
-      return;
-    }
+    // // if (!this.props.onViewportChanged || !map) {
+    // //   return;
+    // // }
 
-    const { viewport } = map;
+    // const { viewport } = map;
+    // console.log('onViewportChanged', map.viewport);
 
-    if (isArray(viewport.center) && isNumber(viewport.zoom)) {
-      this.props.onViewportChanged(viewport.center, viewport.zoom);
-    }
-  };
+    // // if (isArray(viewport.center) && isNumber(viewport.zoom)) {
+    // //   this.props.onViewportChanged(viewport.center, viewport.zoom);
+    // // }
+  }
 
-  onReady = () => {
+  whenMapReady = () => {
     if (this.state.ready) {
       return;
     }
@@ -85,77 +238,62 @@ class MapItem extends PureComponent {
     this.setState({ ready: true }, this.invalidateMapSize);
   };
 
-  // eslint-disable-next-line space-before-function-paren
-  getTileAttribution = async () => {
-    // const { tileServer, tileAttributionMessage } = this.context;
-    const tileServer = null;
-    const tileAttributionMessage = null;
-
+  getTileAttribution = async() => {
     const { map } = this.refs;
-
-    let attributionMessage = tileAttributionMessage;
-    if (!tileServer && map) {
-      const bounds = map.leafletElement.getBounds();
-      const level = map.leafletElement.getZoom();
-
-      attributionMessage = await getHereAttributionMessage(bounds, level);
+    if (this.state.attributionMessage === "") {
+      const attributionMessage = await attachAttribution(map);
+      this.setState({ attributionMessage });
     }
-    this.setState({ attributionMessage });
   };
 
   invalidateMapSize() {
     const { map } = this.refs;
-
-    if (map) {
-      map.leafletElement.invalidateSize();
+    if (!map) {
+      console.error('no map to invalidate?');
+      return;
     }
+
+    map.container.style.height = `${this.props.height}px`;
+    map.container.style.width = `${this.props.width}px`;
+    map.leafletElement.invalidateSize();
   }
 
   /**
-   * static contextType = AppContext;
+   * Compass only needs `<GeoscatterMapItem />` today.
    */
+  renderMapItems() {
+    const { values } = this.props.type;
 
-  renderMapItem() {
-    /* eslint-disable no-unused-vars */
-    const {
-      _id,
-      chartType,
-      data: { documents, metadata },
-      spec,
-    } = this.props;
-
-    const mapRole = find(MAP_ROLES, { name: chartType });
-
-    const geopoints = documents.map(doc => doc.geopoint);
-
-    switch (mapRole) {
-      case GeoSpatialScatterRole:
-        return <GeoscatterMapItem data={geopoints} />;
-      default:
-        return null;
+    if (values.length === 0) {
+      return null;
     }
+
+    const geopoints = values.map(value => {
+      const v = valueToGeoPoint(value);
+      v.fields[0].key = this.props.fieldName;
+      return v;
+    });
+    return <GeoscatterMapItem data={geopoints} />;
   }
 
   render() {
     const { attributionMessage } = this.state;
     const { spec } = this.props;
-
-    // eslint-disable-next-line no-unused-vars
     const { viewport, ...mapProps } = spec;
 
+    // getBoundsZoom(<LatLngBounds> bounds, <Boolean> inside?, <Point> padding?)
+    // Returns the maximum zoom level on which the given bounds fit to the map view in its entirety. If inside (optional) is set to true, the method instead returns the minimum zoom level on which the map view fits into the given bounds in its entirety.
+
+    // className='map' style={{height: this.props.height, width: this.props.width}}
     return (
       <Map
         {...mapProps}
-        preferCanvas
-        whenReady={this.onReady}
+        whenReady={this.whenMapReady.bind(this)}
         ref="map"
         onMoveend={this.getTileAttribution}
       >
-        {this.renderMapItem()}
-        <TileLayer
-          url={DEFAULT_TILE_URL}
-          attribution={attributionMessage}
-        />
+        {this.renderMapItems()}
+        <TileLayer url={DEFAULT_TILE_URL} attribution={attributionMessage} />
       </Map>
     );
   }
