a5823e9753d88da2d7b729b450abb719092239d5
lrlna <shestak.irina@gmail.com>
2019-09-12 13:23:28 +0200

    throw error on analyser try catch

--- src/modules/analyser.js
+++ src/modules/analyser.js
@@ -30,7 +30,7 @@ class Analyser extends Transform {
       this.parser.writeRaw(chunk);
       done();
     } catch (e) {
-      console.log(e);
+      throw new Error(e);
       done(e);
     }
   }
