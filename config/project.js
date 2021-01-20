8ba09899af63f38ccc778455f6d87f3a568cf485
mcasimir <maurizio.cas@gmail.com>
2021-01-20 12:39:22 +0100

    remove react storybook and avoid conflicts on link

--- config/project.js
+++ config/project.js
@@ -17,9 +17,6 @@ module.exports = {
 
     // The path to the electron directory
     electron: path.join(__dirname, '/../electron'),
-
-    // The path to the storybook directory
-    storybook: path.join(__dirname, '/../.storybook')
   },
   plugin: {
     autoprefixer: autoprefixer(`electron >=${electronVersion}`)
